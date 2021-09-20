30d779242b7316570ef9e674e6d28eedf1636bd0
andipextonhome <andi.pexton@googlemail.com>
2021-09-21 00:33:47 +0100

    No More GoTo

--- Amazing/Maze.cs
+++ Amazing/Maze.cs
@@ -19,10 +19,9 @@ namespace Amazing
         {
             var positionHistory = new int[width  * height];
             var maze = new int[width + 1, height + 1];
-            var blocks = CalculateTotalBlocks(width, height);
+            var blocks = CalculateTotalBlocks();
 
-            var Q = 0;
-            var Z = 0;
+            var exitComplete = false;
             var entrance = (int)Random.RND(width);
             var column = entrance;
             var row = 1;
@@ -38,293 +37,61 @@ namespace Amazing
             SetVisited(entrance, 1);
             blocksVisited += 1;
 
-            goto _270;
-            CheckPosition:
-            if (IsNotEndOfRow()) goto AdvanceColumn;
+        /*
+         Possible Actions
 
-            if (IsNotLastRow()) goto NextRow;
-
-
-            column = 1;
-            row = 1;
-            goto CheckRoute;
-            NextRow:
-            (column, row) = StartOfNextRow(row);
-            goto CheckRoute;
-            AdvanceColumn:
-            column = column + 1;
-            AnimationChangeOutput?.DrawFrame(maze, column, row);
-            CheckRoute:
-            if (CurrentBlockIsNotVisited()) goto CheckPosition;
-            _270:
-            if (IsStartOfRow() || BlockToLeftIsVisited()) goto _600;
-            if (IsFirstRow() || BlockAboveIsVisited()) goto _430;
-            if (IsEndOfRow() || BlockToRightIsVisited()) goto _350;
+            Move Up
+            Move Right
+            Move Down
+            Move Left
+            Seek Junction
             
-            var direction = (int) Random.RND(3);
-
-
-            switch (direction)
-            {
-                case 1: goto _940;
-                case 2: goto _980;
-                case 3: goto _1020;
-            }
-
-            _350:
-            if (IsNotLastRow()) goto _380;
-
-            if (Z == 1) goto _410;
-
-            Q = 1;
-            goto _390;
-            _380:
-            if (BlockBelowIsVisited()) goto _410;
-            _390:
-            direction = (int) Random.RND(3);
-
-            switch (direction)
-            {
-                case 1: goto _940;
-                case 2: goto _980;
-                case 3: goto _1090;
-            }
-
-            _410:
-            direction = (int) Random.RND(2);
-
-            switch (direction)
-            {
-                case 1: goto _940;
-                case 2: goto _980;
-            }
-
-            _430:
-            if (column == width) goto _530;
-
-            if (BlockToRightIsVisited()) goto _530;
-
-            if (row != height) goto _480;
-
-            if (Z == 1) goto _510;
-
-            Q = 1;
-            goto _490;
-
-            _480:
-            if (BlockBelowIsVisited()) goto _510;
-            _490:
-            direction = (int) Random.RND(3);
-
-            switch (direction)
-            {
-                case 1: goto _940;
-                case 2: goto _1020;
-                case 3: goto _1090;
-            }
-
-            _510:
-            direction = (int) Random.RND(2);
-
-            switch (direction)
-            {
-                case 1: goto _940;
-                case 2: goto _1020;
-            }
-
-            _530:
-            if (row != height) goto _560;
-
-            if (Z == 1) goto _590;
-
-            Q = 1;
-            goto _570;
-            _560:
-            if (BlockBelowIsVisited()) goto _590;
-            _570:
-            direction = (int) (Random.RND(0) * 2 + 1);
-
-            switch (direction)
-            {
-                case 1: goto _940;
-                case 2: goto _1090;
-            }
-
-            _590:
-            goto _940;
-            _600:
-            if (IsFirstRow()) goto _790;
-
-            if (BlockAboveIsVisited()) goto _790;
-
-            if (IsEndOfRow()) goto _720;
-
-            if (BlockToRightIsVisited()) goto _720;
-
-            if (IsNotLastRow()) goto _670;
-
-            if (Z == 1) goto _700;
-
-            Q = 1;
-            goto _680;
-            _670:
-            if (BlockBelowIsVisited()) goto _700;
-            _680:
-            direction = (int) Random.RND(3);
-
-            switch (direction)
-            {
-                case 1: goto _980;
-                case 2: goto _1020;
-                case 3: goto _1090;
-            }
-
-            _700:
-            direction = (int) Random.RND(2);
-
-            switch (direction)
-            {
-                case 1: goto _980;
-                case 2: goto _1020;
-            }
-
-            _720:
-            if (row != height) goto _750;
-
-            if (Z == 1) goto _780;
-
-            Q = 1;
-            goto _760;
-            _750:
-            if (BlockBelowIsVisited()) goto _780;
-            _760:
-            direction = (int) Random.RND(2);
-
-            switch (direction)
-            {
-                case 1: goto _980;
-                case 2: goto _1090;
-            }
-
-            _780:
-            goto _980;
-
-            _790:
-            if (IsEndOfRow()) goto _880;
-
-            if (BlockToRightIsVisited()) goto _880;
-
-            if (IsNotLastRow()) goto _840;
-
-            if (Z == 1) goto _870;
-
-            Q = 1;
-           // SetCurrentBlockVisited();
-            goto _980;
-            _840:
-            if (BlockBelowIsVisited()) goto _870;
-
-            direction = (int) Random.RND(2);
-
-            switch (direction)
-            {
-                case 1: goto _1020;
-                case 2: goto _1090;
-            }
-
-            _870:
-            goto _1020;
-            _880:
-            if (row != height) goto _910;
-
-            if (Z == 1) goto _930;
-
-            Q = 1;
-            goto _920;
-
-            _910:
-            if (BlockBelowIsVisited()) goto _930;
-            _920:
-            goto _1090;
-            _930:
-            goto CheckPosition;
-            _940:
-            SetBlockToLeftVisited();
-
-   
-            maze[column - 1, row] = 2;
-
-            MoveLeft();
-            if (AllBlocksVisited()) return maze;
-
-            Q = 0;
-            goto _270;
-            _980:
-            SetBlockBelowVisited();
          
-            SetBlockAboveOpenTop();
+         */
 
-            MoveUp();
-            if (AllBlocksVisited()) return maze;
 
-            Q = 0;
-            goto _270;
+        while (!AllBlocksVisited())
+        {
+            var NextAction = GetNextAction();
 
-            _1020:
-            SetBlockToRightVisited();
+            switch (NextAction)
+            {
+                case NextAction.MoveUp:
+                    KnockThroughAndMoveUp();
+                    break;
+                case NextAction.MoveRight:
+                    if (IsLastRow())
+                    {
+                        SetExitComplete();
+                        SetBlockOpenTop();
+                        FindJunction();
+                    }
+                    else
+                    {
+                        SetBlockAboveVisited();
+                        SetBlockOpenTop();
+                        MoveRight();
+                    }
+                    break;
+                case NextAction.MoveDown:
+                    SetBlockToRightVisited();
+                    SetCurrentBlockOpenRight();
+                    MoveDown();
+                    break;
+                case NextAction.MoveLeft:
+                    KnockThroughAndMoveLeft();
+                    break;
+                case NextAction.FindJunction:
+                    FindJunction();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        return maze;
             
-   
-            if (maze[column, row] == 0) goto _1050;
-            maze[column, row] = 3;
-
-            goto _1060;
-
-            _1050:
-            maze[column, row] = 2;
-
-            _1060:
-            MoveDown();
-            if (AllBlocksVisited()) return maze;
-
-            goto _600;
-            _1090:
-            if (Q == 1) goto Set_Z;
-
-            SetBlockAboveVisited();
-            
-            if (CurrentPositionBothClosed()) goto _1120;
-
-            SetBlockOpenBoth();
-
-            goto _1130;
-            _1120:
-            SetBlockOpenTop();
-
-        _1130:
-            MoveRight();
-            if (AllBlocksVisited()) return maze;
-
-            goto _270;
-            Set_Z:
-            Z = 1;
-
-            if (CurrentPositionBothClosed()) goto Set_Block_Open_Top_Reset_To_Start;
-
-            SetBlockOpenBoth();
-
-            Q = 0;
-            goto CheckPosition;
-            Set_Block_Open_Top_Reset_To_Start:
-            SetBlockOpenTop();
-
-            Q = 0;
-            column = 1;
-            row = 1;
-            goto CheckRoute;
-
-
-           
-
+            /////////////////////////////////////////
             void MoveRight()
             {
                 row = row + 1;
@@ -351,30 +118,30 @@ namespace Amazing
 
             bool CurrentBlockIsNotVisited()
             {
-                return !GetPositionHistory(column, row) ;
+                return !GetPositionHistoryVisited(column, row) ;
             }
 
             bool BlockToLeftIsVisited()
             {
-                return GetPositionHistory(column - 1, row);
+                return GetPositionHistoryVisited(column - 1, row);
             }
 
             bool BlockAboveIsVisited()
             {
-                return GetPositionHistory(column, row - 1);
+                return GetPositionHistoryVisited(column, row - 1);
             }
 
             bool BlockToRightIsVisited()
             {
-                return GetPositionHistory(column + 1, row);
+                return GetPositionHistoryVisited(column + 1, row);
             }
 
             bool BlockBelowIsVisited()
             {
-                return GetPositionHistory(column, row + 1);
+                return GetPositionHistoryVisited(column, row + 1);
             }
 
-            bool GetPositionHistory(int x, int y)
+            bool GetPositionHistoryVisited(int x, int y)
             {
                 return positionHistory[CalculatePositionForHistory(x, y)] != 0;
             }
@@ -409,19 +176,9 @@ namespace Amazing
                 blocksVisited = blocksVisited + 1;
             }
 
-            bool CurrentPositionBothClosed()
-            {
-                return maze[column, row] == 0;
-            }
-
             void SetBlockOpenTop()
             {
-                maze[column, row] = 1;
-            }
-
-            void SetBlockOpenBoth()
-            {
-                maze[column, row] = 3;
+                maze[column, row] = maze[column, row] | 1;
             }
 
             void SetBlockAboveOpenTop()
@@ -463,17 +220,223 @@ namespace Amazing
             {
                 return column != width;
             }
+
+            void SetToBeginning()
+            {
+                column = 1;
+                row = 1;
+            }
+
+            void SetExitComplete()
+            {
+                exitComplete = true;
+            }
+
+            bool IsLastRow()
+            {
+                return !IsNotLastRow();
+            }
+
+            void SetBlockToLeftOpenRight()
+            {
+                maze[column - 1, row] = 2;
+            }
+
+            void KnockThroughAndMoveLeft()
+            {
+                SetBlockToLeftVisited();
+                SetBlockToLeftOpenRight();
+                MoveLeft();
+            }
+
+            void KnockThroughAndMoveUp()
+            {
+                SetBlockBelowVisited();
+                SetBlockAboveOpenTop();
+                MoveUp();
+            }
+
+            void SetCurrentBlockOpenRight()
+            {
+                maze[column, row] = maze[column, row] | 2;
+            }
+
+            void SetToBeginningOfNextRow()
+            {
+                column = 1;
+                row += 1;
+                AnimationChangeOutput?.DrawFrame(maze, column, row);
+            }
+
+            int CalculateTotalBlocks()
+            {
+                return width * height + 1;
+            }
+
+            void FindJunction()
+            {
+                do
+                {
+                    if (IsNotEndOfRow())
+                        MoveDown();
+                    else if (IsNotLastRow())
+                        SetToBeginningOfNextRow();
+                    else
+                        SetToBeginning();
+                } while (CurrentBlockIsNotVisited());
+            }
+
+            NextAction GetNextAction()
+            {
+                if (IsStartOfRow() || BlockToLeftIsVisited())
+                {
+                    if (IsFirstRow() || BlockAboveIsVisited())
+                    {
+                        if (IsEndOfRow() || BlockToRightIsVisited())
+                        {
+                            if (IsNotLastRow() && BlockBelowIsVisited() || !IsNotLastRow() && exitComplete)
+                                return NextAction.FindJunction; // goto FindJunction;
+                            return NextAction.MoveRight; // goto MoveRight;
+                        }
+
+                        if (IsNotLastRow())
+                        {
+                            if (BlockBelowIsVisited()) return NextAction.MoveDown; // goto MoveDown;
+
+
+                            switch ((int) Random.RND(2))
+                            {
+                                case 1: return NextAction.MoveDown; // goto MoveDown;
+                                case 2: return NextAction.MoveRight; // goto MoveRight;
+                                default:
+                                    throw new ArgumentOutOfRangeException();
+                            }
+                        }
+
+                        if (exitComplete) return NextAction.MoveDown; // goto MoveDown;
+
+                        return NextAction.MoveUp; // goto MoveUp;
+                    }
+
+                    ;
+
+                    if (IsEndOfRow() || BlockToRightIsVisited())
+                    {
+                        if (IsNotLastRow() && BlockBelowIsVisited() || !IsNotLastRow() && exitComplete) 
+                            return NextAction.MoveUp; // goto MoveUp;
+
+                        switch ((int) Random.RND(2))
+                        {
+                            case 1: return NextAction.MoveUp; // goto MoveUp;
+                            case 2: return NextAction.MoveRight; // goto MoveRight;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+
+                    if (IsNotLastRow() && BlockBelowIsVisited() || !IsNotLastRow() && exitComplete)
+                    {
+                        switch ((int) Random.RND(2))
+                        {
+                            case 1: return NextAction.MoveUp; // goto MoveUp;
+                            case 2: return NextAction.MoveDown; // goto MoveDown;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+
+                    switch ((int) Random.RND(3))
+                    {
+                        case 1: return NextAction.MoveUp; // goto MoveUp;
+                        case 2: return NextAction.MoveDown; // goto MoveDown;
+                        case 3: return NextAction.MoveRight; // goto MoveRight;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+
+                if (IsFirstRow() || BlockAboveIsVisited())
+                {
+                    if (IsEndOfRow() || BlockToRightIsVisited())
+                    {
+                        if (IsNotLastRow() && BlockBelowIsVisited() || !IsNotLastRow() && exitComplete) 
+                            return NextAction.MoveLeft; // goto MoveLeft;
+
+                        switch ((int) (Random.RND(0) * 2 + 1))
+                        {
+                            case 1: return NextAction.MoveLeft; // goto MoveLeft;
+                            case 2: return NextAction.MoveRight; // goto MoveRight;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+
+                    if (IsNotLastRow() && BlockBelowIsVisited() || !IsNotLastRow() && exitComplete)
+                    {
+                        switch ((int) Random.RND(2))
+                        {
+                            case 1: return NextAction.MoveLeft; // goto MoveLeft;
+                            case 2: return NextAction.MoveDown; // goto MoveDown;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+
+                    switch ((int) Random.RND(3))
+                    {
+                        case 1: return NextAction.MoveLeft; // goto MoveLeft;
+                        case 2: return NextAction.MoveDown; // goto MoveDown;
+                        case 3: return NextAction.MoveRight; // goto MoveRight;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+
+                if (IsEndOfRow() || BlockToRightIsVisited())
+                {
+                    if (IsNotLastRow() && BlockBelowIsVisited() || !IsNotLastRow() && exitComplete)
+                    {
+                        switch ((int) Random.RND(2))
+                        {
+                            case 1: return NextAction.MoveLeft; // goto MoveLeft;
+                            case 2: return NextAction.MoveUp; // goto MoveUp;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+
+                    switch ((int) Random.RND(3))
+                    {
+                        case 1: return NextAction.MoveLeft; // goto MoveLeft;
+                        case 2: return NextAction.MoveUp; // goto MoveUp;
+                        case 3: return NextAction.MoveRight; // goto MoveRight;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+
+                var direction = (int) Random.RND(3);
+
+
+                switch (direction)
+                {
+                    case 1: return NextAction.MoveLeft; // goto MoveLeft;
+                    case 2: return NextAction.MoveUp; // goto MoveUp;
+                    case 3: return NextAction.MoveDown; // goto MoveDown;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
         }
 
-        private static int CalculateTotalBlocks(int width, int height)
-        {
-            return width * height + 1;
-        }
+    }
 
-
-        private static (int, int) StartOfNextRow(int row)
-        {
-            return (1, row + 1);
-        }
+    public enum NextAction
+    {
+        MoveUp,
+        MoveRight,
+        MoveDown,
+        MoveLeft,
+        FindJunction
     }
 }
